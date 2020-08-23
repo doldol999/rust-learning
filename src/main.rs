ba9b7dde3b25dc0698cc8b35ccfbd18cf39a66a6
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2020-08-23 20:16:31 +0800

    Added sample for as and use keyword

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod ownership;
 mod structs;
 mod enums;
 
+use enums::enums::execute as executeEnums; // using as keyword to replace execute with executeEnums
+
 fn main() {
     // variables::variables::execute();
     // guessing_game::guessing_game::execute();
     // ownership::ownership::execute();
     // structs::structs::execute();
-    enums::enums::execute();
+
+    // enums::enums::execute(); // relative path
+    // crate::enums::enums::execute(); // absolute path
+    executeEnums(); // using use keyword
 }
