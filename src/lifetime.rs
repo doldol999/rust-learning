f642af227037ad57fb29dff7b1e274f7491bef07
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2020-09-03 21:40:13 +0800

    Finalized demos for lifetimes

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -1,7 +1,24 @@
 pub mod lifetime {
+  // Lifetime annotations
+  // &i32        // a reference
+  // &'a i32     // a reference with an explicit lifetime
+  // &'a mut i32 // a mutable reference with an explicit lifetime
+
+  // Lifetime annotations in function signatures
+  fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+      return x;
+    } else {
+      return y;
+    }
+  }
+
+  
   pub fn execute() {
     let f;
-    
+    let outer_str = "hello";
+    let largest_str: &str;
+
     { 
       // this is just an artificial scope 
       // to demonstrate lifetime
@@ -12,8 +29,13 @@ pub mod lifetime {
       // `x`does not live long enough
 
       f = x; // this works because f here copies the value of x and is not referenced ( & )
+    
+      let inner_str = "world";
+  
+      largest_str = longest(&inner_str, &outer_str);
     }
-
+    
     println!("{}", f);
+    println!("{}", largest_str);
   }
 }
