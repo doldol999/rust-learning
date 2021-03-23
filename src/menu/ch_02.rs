9f0d3340acfa4e546dda4ca9ec435cf0b433d48e
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2021-03-24 02:11:04 +0800

    fix: Improve guessing game guess validation

--- src/menu/ch_02.rs
+++ src/menu/ch_02.rs
@@ -3,6 +3,24 @@ pub mod guessing_game {
   use std::cmp::Ordering;
   use rand::Rng;
 
+  struct Guess {
+    value: i32,
+  }
+
+  impl Guess {
+    pub fn new(value: i32) -> Guess {
+      if value < 1 || value > 100 {
+        panic!("You can only guess numbers from 1 to 100");
+      }
+
+      Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+      self.value
+    }
+  }
+
   #[allow(dead_code)] // trait needed to allow unused code
   pub fn play() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -19,13 +37,13 @@ pub mod guessing_game {
 
       tries += 1;
 
-      let guess: u32 = guess.trim().parse().expect("Please type a number!");
+      let guess: Guess = Guess::new(guess.trim().parse().expect("Please type a number!"));
 
-      match guess.cmp(&secret_number) {
-        Ordering::Less => println!("{} is too small!", guess),
-        Ordering::Greater => println!("{} is too big!", guess),
+      match guess.value().cmp(&secret_number) {
+        Ordering::Less => println!("{} is too small!", guess.value()),
+        Ordering::Greater => println!("{} is too big!", guess.value()),
         Ordering::Equal => {
-          println!("{} is correct! You had {} tries!", guess, tries);
+          println!("{} is correct! You had {} tries!", guess.value(), tries);
           break;
         },
       }
